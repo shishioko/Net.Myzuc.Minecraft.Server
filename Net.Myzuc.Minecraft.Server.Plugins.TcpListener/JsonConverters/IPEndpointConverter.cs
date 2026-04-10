3032e2ae671f22f65d0ffb6750ab79c0f15e5dce
shishioko <shishioko@proton.me>
2026-04-10 14:32:49 +0200

    adjsut visibility

--- Net.Myzuc.Minecraft.Server.Plugins.TcpListener/JsonConverters/IPEndpointConverter.cs
+++ Net.Myzuc.Minecraft.Server.Plugins.TcpListener/JsonConverters/IPEndpointConverter.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 
 namespace Net.Myzuc.Minecraft.Server.Plugins.TcpListener.JsonConverters
 {
-    public class IPEndPointConverter : JsonConverter<IPEndPoint>
+    internal class IPEndPointConverter : JsonConverter<IPEndPoint>
     {
         public override IPEndPoint? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
